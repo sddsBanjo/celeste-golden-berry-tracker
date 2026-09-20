47b972eac7f0f92000de4c6bd8abc50100a4dfe4
SDDS Banjo <168935997+sddsBanjo@users.noreply.github.com>
2026-09-20 10:47:27 -0300

    chore: add json body parsing middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ process.loadEnvFile();
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(express.json());
 
 const apiRouter = Router();
 apiRouter.get("/health", (req, res) => {
