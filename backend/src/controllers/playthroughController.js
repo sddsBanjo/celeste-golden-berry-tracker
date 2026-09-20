97a846306faff47d79a00a7bc6381fb9417dcc7b
SDDS Banjo <168935997+sddsBanjo@users.noreply.github.com>
2026-09-20 17:36:18 -0300

    feat: add get playthrough by id endpoint

--- backend/src/controllers/playthroughController.js
+++ backend/src/controllers/playthroughController.js
@@ -5,6 +5,19 @@ export function getAll(req, res) {
     res.json(playthroughs);
 }
 
+export function getById(req, res) {
+    const id = Number(req.params.id);
+
+    const result = playthroughs.find((playthrough) => playthrough.id === id);
+    if (!result) {
+        return res.status(404).json({
+            error: `Couldn't find a playthrough of ID ${id}.`
+        });
+    }
+
+    res.json(result);
+}
+
 export function create(req, res) {
     const name = req.body.name.trim();
     const description = (req.body.description ?? "").trim(); 
--- backend/src/routes/playthroughRoutes.js
+++ backend/src/routes/playthroughRoutes.js
@@ -4,6 +4,7 @@ import { validatePlaythrough } from "../middlewares/validatePlaythrough.js";
 
 const router = Router();
 router.get("/", playthroughController.getAll);
+router.get("/:id", playthroughController.getById);
 router.post("/", validatePlaythrough, playthroughController.create);
 
 export default router;
